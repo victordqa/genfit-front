74ae6db30ac335334f03e54753d606059df56534
Victor <victorqalves@gmail.com>
2023-07-19 14:07:06 -0400

    basic layout and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-lg lg:flex text-blue-400">
         Hello!!!
       </div>
