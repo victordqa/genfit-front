36cc4374b4f909d35ccee46012f9a92b8d266be9
Victor <victorqalves@gmail.com>
2023-08-03 14:39:49 -0400

    useGet hook

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -1,4 +1,3 @@
-"use client"
 import axios from "axios"
 
 export const usePost = async (
@@ -14,5 +13,44 @@ export const usePost = async (
     console.log(error)
 
     return { error }
+  }
+}
+
+export const useGet = async (url: string) => {
+  try {
+    const res = await axios.get(url, { withCredentials: true })
+    console.log(res)
+
+    return {
+      data: res.data,
+      status: res.status,
+      headers: res.headers,
+      message: "",
+    }
+  } catch (error: any) {
+    console.log(error)
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+
+      return {
+        data: error.response.data,
+        status: error.response.status,
+        headers: error.response.headers,
+        message: error.message,
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      return {
+        message: "Sem resposta do servidor",
+      }
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      return {
+        message: error.message,
+      }
+    }
   }
 }
