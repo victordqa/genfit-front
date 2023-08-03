a0c2657c47d5f2e34582ac5977e3f6e23ca0e1e8
Victor <victorqalves@gmail.com>
2023-08-03 14:39:23 -0400

    check auth and set global states

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,9 +5,13 @@ import {
   ReactNode,
   SetStateAction,
   createContext,
+  useEffect,
   useState,
 } from "react"
-
+import { useGet } from "../hooks/useHttp"
+import routes from "../routes"
+import { publicRoutes } from "../routes"
+import { usePathname, useRouter } from "next/navigation"
 interface AuthState {
   coachData: { name: string; email: string; id: number } | null
 }
@@ -23,6 +27,31 @@ export const AuthenticationContext = createContext<AuthStateWithSet>({
 
 export default function AuthContext({ children }: { children: ReactNode }) {
   const [auth, setAuth] = useState<AuthState>({ coachData: null })
+
+  const pathname = usePathname()
+  const { push } = useRouter()
+
+  //make a call to me api
+  //set state based on call - null if unauthorized and data if authorized
+  useEffect(() => {
+    console.log("effect")
+    useGet(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${routes.me}`).then(
+      (res) => {
+        const statusCode = res.status
+
+        if (statusCode === 200) {
+          const { name, email, id } = res.data.coach
+          // set global states
+          setAuth({ coachData: { name, email, id } })
+        } else {
+          console.log(res)
+          if (!publicRoutes.includes(pathname)) {
+            push(routes.home)
+          }
+        }
+      }
+    )
+  }, [])
 
   return (
     <AuthenticationContext.Provider value={{ ...auth, setAuth }}>
