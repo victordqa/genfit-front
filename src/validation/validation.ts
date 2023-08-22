fc0eb05067e5f0ec5156bc2a7aa5f130c97f65e7
Victor <victorqalves@gmail.com>
2023-08-22 10:11:49 -0400

    create box validation

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -9,12 +9,26 @@ type SignUpFormInput = {
   password_confirmation: string
 }
 
+type CreateBoxFormInput = {
+  name: string
+}
+
 export const singUpFormValidation = (input: SignUpFormInput) => {
   const rules = {
     name: "required|min:1|max:255",
     email: "required|email",
     password: "required|min:6|max:15|confirmed",
     password_confirmation: "required|min:6|max:15",
+  }
+
+  let validation = new Validator(input, rules)
+  validation.passes()
+  return validation.errors.all()
+}
+
+export const createBoxFormValidation = (input: CreateBoxFormInput) => {
+  const rules = {
+    name: "required|min:1|max:255",
   }
 
   let validation = new Validator(input, rules)
